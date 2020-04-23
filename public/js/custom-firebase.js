30d3a02a259e1fcde00ab0ccffd98534540276a3
valor <valor312@gmail.com>
2020-04-24 06:36:56 +0800

    4/24 updated

--- public/js/custom-firebase.js
+++ public/js/custom-firebase.js
@@ -4,8 +4,9 @@
     // console.log('custom-firebase.js');
 
     $(document).ready(function () {
-        let users = [];
-        let devices = [];
+        let users = {};
+        let devices = {};
+        let games = {};
 
         const showFirestoreDatabase = () => {
             // console.log('Custom Firebase');
@@ -17,10 +18,10 @@
             // This is reference to tha database, to fetch data from database use below code:
 
             var usersRef = database.ref('v2/users');
-            
+
             usersRef.on('value', function (snapshot) {
                 users = snapshot.val();
-                
+
                 displayTable();
             });
 
@@ -29,72 +30,76 @@
                 devices = snapshot.val();
 
                 displayTable();
+            });
 
-                // debugger
+            var gamesRef = database.ref('v2/games');
+            gamesRef.on('value', function (snapshot) {
+                games = snapshot.val();
+
+                displayTable();
             });
 
         }
 
         const displayTable = () => {
-            // console.log('displayTable()')
-            // console.log(users);
-            // console.log(devices);
-
-            if (users.length == 0 || devices.length == 0)
+            // users
+            if (Object.keys(users).length == 0)
                 return;
 
+            const platform = {};
+            const qty_games = {};
+
             let html = '<table>';
+            html += '<tr>';
+            html += '<th></th>';
+            html += '<th></th>';
+            html += '<th>Screen Name</th>';
+            html += '<th>Player ID</th>';
+            html += '<th>Platform</th>';
+            html += '<th>Date First Appeared</th>';
+            html += '<th>Qty of Games Played</th>';
+            html += '<th>Qty of Referrals</th>';
+            html += '</tr>';
+
+            // devices
+            if (Object.keys(devices).length > 0) {
+                for (const key in devices) {
+                    const device = devices[key];
+
+                    platform[device.PlayerID] = device.DevicePlatform || '';
+                    platform[device.PlayerID] = platform[device.PlayerID].replace('IPhonePlayer', 'iPhone');
+                    qty_games[device.PlayerID] = qty_games[device.PlayerID] ? qty_games[device.PlayerID] + 1 : 1;
+                }
+            }
+
+            // console.log(qty_games);
+            // console.log(platform);
+            // debugger;
+
+            // users
+            for (const key in users) {
+                const user = users[key];
+
                 html += '<tr>';
-                html += '<th></th>';
-                html += '<th></th>';
-                html += '<th>Screen Name</th>';
-                html += '<th>Player ID</th>';
-                html += '<th>Platform</th>';
-                html += '<th>Date First Appeared</th>';
-                html += '<th>Qty of Games Played</th>';
-                html += '<th>Qty of Referrals</th>';
+                html += '<td><img src="' + user.FlagURL + '" width="50" height="50" /></td>';
+                html += '<td><img src="' + user.AvatarURL + '" width="50" height="50" /></td>';
+                html += '<td>' + user.DisplayName + '</td>';
+                html += '<td>' + key + '</td>';
+                html += '<td>' + (platform[key] || '') + '</td>';
+                html += '<td>' + moment(user.Timestamp).format('MM-DD HH:mm:ss') + '</td>';
+                html += '<td>' + (qty_games[key] || 0) + '</td>';
+                html += '<td>' + 0 + '</td>';
                 html += '</tr>';
 
-                jQuery.each(users, function (key, value) {
-                    // You can put condition to filter your value
-                    // and it won't show on the frontend
 
-                    // const date = timeConverter(value.Timestamp);
-                    // console.log(date);
-
-                    // debugger
-                    
-                    html += '<tr>';
-                    html += '<td><img src="' + value.FlagURL + '" width="50" height="50" /></td>';
-                    html += '<td><img src="' + value.AvatarURL + '" width="50" height="50" /></td>';
-                    html += '<td>' + value.DisplayName + '</td>';
-                    html += '<td>' + key + '</td>';
-                    html += '<td>' + 'Android/iPhone' + '</td>';
-                    html += '<td>' + moment(value.Timestamp).format('MM-DD HH:mm:ss') + '</td>';
-                    html += '<td>' + 0 + '</td>';
-                    html += '<td>' + 0 + '</td>';
-                    html += '</tr>';
+            }
 
 
-                })
-
-                const firestoreEl = jQuery('#custom-firebase');
-                html += '</table>';
-                firestoreEl.html(html)
+            // display
+            const firestoreEl = jQuery('#custom-firebase');
+            html += '</table>';
+            firestoreEl.html(html)
         }
-
-        function timeConverter(UNIX_timestamp){
-            var a = new Date(UNIX_timestamp * 1000);
-            var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-            var year = a.getFullYear();
-            var month = months[a.getMonth()];
-            var date = a.getDate();
-            var hour = a.getHours();
-            var min = a.getMinutes();
-            var sec = a.getSeconds();
-            var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
-            return time;
-          }
 
         showFirestoreDatabase()
     })
